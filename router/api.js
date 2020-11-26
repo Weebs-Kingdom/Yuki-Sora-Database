dc6df466ba7c04d67c28f412c31437983aed9b1a
Noah Till <ntill@gmx.de>
2020-11-26 17:15:09 +0100

    Update api.js

--- router/api.js
+++ router/api.js
@@ -9,7 +9,6 @@ const Serverd = require("../models/Server");
 const Monster = require("../models/Monster/Monster");
 const verify = require("../middleware/verifyApiToken");
 
-
 //create ai monster usw.
 router.post("/createFight", verify, async(req, res) => {
     res.status(200).json({ message: "test looolz UWU OWO :P" });
@@ -19,53 +18,63 @@ router.post("/createFight", verify, async(req, res) => {
 router.post("/fight", verify, async(req, res) => {
     const m1 = req.body.monster;
     const dmg = req.body.dmg;
+
+    if (!m1 || !dmg)
+        return res
+            .status(400)
+            .json({ status: 400, message: "Request is mssing arguments" });
+
     var monster1 = await UserMonster.findById(m1);
-    if (monster1) return res.status(400).json({ status: 400, message: "Monster not found!" });
+    if (!monster1)
+        return res.status(400).json({ status: 400, message: "Monster not found!" });
     monster1.hp = monster1.hp - dmg;
     monster1 = await monster1.save();
-    res.status(200).json({ status: "200", data: [monster1] })
+    res.status(200).json({ status: "200", data: [monster1] });
 });
 
-router.post("/getUserByDID", verify, async(req, res) => {
-    const s = req.body.id;
+router.post("/getUser", verify, async(req, res) => {
+    const user = getUser(req.body);
 
-    const user = getUserFromDID(s);
-    if (user) return res.status(400).json({ status: 400, message: "User not found!" });
+    if (!user)
+        return res.status(400).json({ status: 400, message: "User not found!" });
     //maybe to this in more specific json text yk...
-    res.status(200).json({ status: "200", data: [user] });
+    res.status(200).json({ status: "200", data: user });
 });
 
 router.post("/getUserInventoryByDID", verify, async(req, res) => {
-    const s = req.body.id;
-    const user = getUserFromDID(s);
+    const user = getUser(req.body);
+    if (!user)
+        return res.status(400).json({ status: 400, message: "User not found!" });
     var inventory = await ItemUserCon.find({ user: user._id });
-    if (inventory) return res.status(400).json({ status: 400, message: "Inventory not found!" });
-    var returningString = "{status: \"200\", data: [";
+    if (!inventory)
+        return res
+            .status(400)
+            .json({ status: 400, message: "Inventory not found!" });
+    var returningString = '{status: "200", data: [';
     var first = true;
-    inventory.forEach(element => {
-        if (!first)
-            returningString += ",";
-        else
-            first = false;
+    inventory.forEach((element) => {
+        if (!first) returningString += ",";
+        else first = false;
 
-        returningString += "{item: " + element.item + ", amount: " + element.amount + "}";
+        returningString +=
+            "{item: " + element.item + ", amount: " + element.amount + "}";
     });
     returningString += "]}";
     res.status(200).json(returningString);
 });
 
 router.post("/getUserMonstersByDID", verify, async(req, res) => {
-    const s = req.body.id;
-    const user = getUserFromDID(s);
+    const user = getUser(req.body);
+    if (!user)
+        return res.status(400).json({ status: 400, message: "User not found!" });
     var monsters = await UserMonster.find({ user: user._id });
-    if (monsters) return res.status(400).json({ status: 400, message: "Monster not found!" });
-    var returningString = "{status: \"200\", data: [";
+    if (!monsters)
+        return res.status(400).json({ status: 400, message: "Monster not found!" });
+    var returningString = '{status: "200", data: [';
     var first = true;
-    monsters.forEach(element => {
-        if (!first)
-            returningString += ",";
-        else
-            first = false;
+    monsters.forEach((element) => {
+        if (!first) returningString += ",";
+        else first = false;
 
         returningString += "{monster: " + element + "}";
     });
@@ -74,22 +83,31 @@ router.post("/getUserMonstersByDID", verify, async(req, res) => {
 });
 
 router.post("/userItem", verify, async(req, res) => {
-    const s = req.body.id;
     const si = req.body.item;
     const amount = req.body.amount;
     const item = Item.findById(si);
-    const user = getUserFromDID(s);
+    const user = getUser(req.body);
+    if (!user)
+        return res.status(400).json({ status: 400, message: "User not found!" });
+    if (!item)
+        return res.status(400).json({ status: 400, message: "Item not found!" });
+
     const storage = new ItemUserCon({
         item: si._id,
         user: user._id,
-        amount: amount
+        amount: amount,
     });
+
     try {
         const savedItem = await storage.save();
         res.status(200).json({ status: 200, message: savedItem._id });
     } catch (err) {
         console.log("an error occured! " + err);
-        res.status(400).json({ status: 400, message: "error while creating new user!", error: err });
+        res.status(400).json({
+            status: 400,
+            message: "error while creating new user!",
+            error: err,
+        });
     }
 });
 
@@ -100,17 +118,25 @@ router.post("/user", verify, async(req, res) => {
         res.status(200).json({ status: 200, message: savedUser._id });
     } catch (err) {
         console.log("an error occured! " + err);
-        res.status(400).json({ status: 400, message: "error while creating new user!", error: err });
+        res.status(400).json({
+            status: 400,
+            message: "error while creating new user!",
+            error: err,
+        });
     }
 });
 
 router.patch("/user", verify, async(req, res) => {
     try {
-        const savedUser = await User.update({ _id: req.body._id }, (req.body.data));
+        const savedUser = await User.update({ _id: req.body._id }, req.body.data);
         res.status(200).json({ status: 200, message: savedUser._id });
     } catch (err) {
         console.log("an error occured! " + err);
-        res.status(400).json({ status: 400, message: "error while patching new user!", error: err });
+        res.status(400).json({
+            status: 400,
+            message: "error while patching new user!",
+            error: err,
+        });
     }
 });
 
@@ -120,7 +146,9 @@ router.delete("/user", verify, async(req, res) => {
         res.status(200).json({ status: 200, message: "removed" });
     } catch (err) {
         console.log("an error occured! " + err);
-        res.status(400).json({ status: 400, message: "error while deleting user!", error: err });
+        res
+            .status(400)
+            .json({ status: 400, message: "error while deleting user!", error: err });
     }
 });
 
@@ -131,17 +159,27 @@ router.post("/monster", verify, async(req, res) => {
         res.status(200).json({ status: 200, message: savedMonster._id });
     } catch (err) {
         console.log("an error occured! " + err);
-        res.status(400).json({ status: 400, message: "error while creating new monster!", error: err });
+        res.status(400).json({
+            status: 400,
+            message: "error while creating new monster!",
+            error: err,
+        });
     }
 });
 
 router.patch("/monster", verify, async(req, res) => {
     try {
-        const savedMonster = await Monster.update({ _id: req.body._id }, (req.body.data));
+        const savedMonster = await Monster.update({ _id: req.body._id },
+            req.body.data
+        );
         res.status(200).json({ status: 200, message: savedMonster._id });
     } catch (err) {
         console.log("an error occured! " + err);
-        res.status(400).json({ status: 400, message: "error while patching new monster!", error: err });
+        res.status(400).json({
+            status: 400,
+            message: "error while patching new monster!",
+            error: err,
+        });
     }
 });
 
@@ -151,7 +189,11 @@ router.delete("/monster", verify, async(req, res) => {
         res.status(200).json({ status: 200, message: "removed" });
     } catch (err) {
         console.log("an error occured! " + err);
-        res.status(400).json({ status: 400, message: "error while deleting monster!", error: err });
+        res.status(400).json({
+            status: 400,
+            message: "error while deleting monster!",
+            error: err,
+        });
     }
 });
 
@@ -162,17 +204,25 @@ router.post("/item", verify, async(req, res) => {
         res.status(200).json({ status: 200, message: savedItem._id });
     } catch (err) {
         console.log("an error occured! " + err);
-        res.status(400).json({ status: 400, message: "error while creating new item!", error: err });
+        res.status(400).json({
+            status: 400,
+            message: "error while creating new item!",
+            error: err,
+        });
     }
 });
 
 router.patch("/item", verify, async(req, res) => {
     try {
-        const cItem = await Item.update({ _id: req.body._id }, (req.body.data));
+        const cItem = await Item.update({ _id: req.body._id }, req.body.data);
         res.status(200).json({ status: 200, message: savedItem._id });
     } catch (err) {
         console.log("an error occured! " + err);
-        res.status(400).json({ status: 400, message: "error while patching new item!", error: err });
+        res.status(400).json({
+            status: 400,
+            message: "error while patching new item!",
+            error: err,
+        });
     }
 });
 
@@ -182,7 +232,9 @@ router.delete("/item", verify, async(req, res) => {
         res.status(200).json({ status: 200, message: "removed" });
     } catch (err) {
         console.log("an error occured! " + err);
-        res.status(400).json({ status: 400, message: "error while deleting item!", error: err });
+        res
+            .status(400)
+            .json({ status: 400, message: "error while deleting item!", error: err });
     }
 });
 
@@ -192,6 +244,17 @@ async function getUserFromDID(did) {
 
 async function getUserDidFromId(id) {
     return await User.findOne({ _id: id }).userID;
+}
+
+async function getUser(body) {
+    const s = body.id;
+    const si = body._id;
+    var user;
+
+    if (s) user = getUserFromDID(s);
+    if (si) user = await User.findById(si);
+
+    return user;
 }
 
 module.exports = router;
