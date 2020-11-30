93675ed7649bb526f5901fbf172e03d5440e9901
Noah Till <ntill@gmx.de>
2020-11-30 09:27:34 +0100

    Update verifyApiToken.js

--- middleware/verifyApiToken.js
+++ middleware/verifyApiToken.js
@@ -2,7 +2,7 @@ var ApiToken = require('../models/ApiToken');
 
 module.exports = async function(req, res, next) {
     const token = req.header("api-token");
-    const apiToken = ApiToken.findOne({ token: token });
+    const apiToken = await ApiToken.findOne({ token: token });
     if (!apiToken || apiToken.token != token) return res.status(401).json({ status: 401, message: "Access Denied! Invalid acces token!" });
 
     next();
