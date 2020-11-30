fa9291faf926283ec6251ba7ec44e171a1e1d04d
Noah Till <ntill@gmx.de>
2020-11-30 09:22:43 +0100

    Update verifyApiToken.js

--- middleware/verifyApiToken.js
+++ middleware/verifyApiToken.js
@@ -3,7 +3,7 @@ var ApiToken = require('../models/ApiToken');
 module.exports = async function(req, res, next) {
     const token = req.header("api-token");
     const apiToken = ApiToken.findOne({ token: token });
-    if (!apiToken) return res.status(401).json({ status: 401, message: "Access Denied! Invalid acces token!" });
+    if (!apiToken || apiToken.token != token) return res.status(401).json({ status: 401, message: "Access Denied! Invalid acces token!" });
 
     next();
 };
