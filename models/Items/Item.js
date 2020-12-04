30d9805f02edc64e75eef4be2e5039bf56cdd4e4
Noah Till <ntill@gmx.de>
2020-12-04 12:37:45 +0100

    Update Item.js

--- models/Items/Item.js
+++ models/Items/Item.js
@@ -5,6 +5,8 @@ const userSchema = new mongoose.Schema({
     itemRarity: { type: String, require: true },
     itemImageURL: { type: String, require: true },
     itemDescription: { type: String, require: true },
+    itemType: { type: String, require: true },
+    itemCanFound: { type: Boolean, default: false },
     isitemCookable: { type: Boolean, require: true },
     cooksInto: { type: mongoose.Schema.Types.ObjectId, require: true }
 });
